5b16b7648ed969035869a2be108d769e2880e984
Pedro Nunes <pedroj.nunes0@gmail.com>
2019-03-17 11:58:47 -0300

    remove id para inserçao de ordens

--- build/routes/order.route.js
+++ build/routes/order.route.js
@@ -9,6 +9,6 @@ var orderController = require('../controllers/order.controller');
 router.get('/', orderController.get);
 router.get('/:id', orderController.getById);
 router.post('/', orderController.post);
-router.put('/:id', orderController.put);
+router.put('/', orderController.put);
 router.delete('/:id', orderController.delete);
 module.exports = router;
