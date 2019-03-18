2b2264921bb51bf702c620ff5cd1f6872975a987
Pedro Nunes <pedroj.nunes0@gmail.com>
2019-03-18 15:12:20 -0300

    adiciona tabela de customer e faz JOIN na query de getOrders

--- build/dao/order.dao.js
+++ build/dao/order.dao.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var sqlite = require('sqlite-sync');
 sqlite.connect('chef-em-casa.db');
 function getOrders() {
-    var rows = sqlite.run("SELECT * FROM orders");
+    var rows = sqlite.run("SELECT * FROM orders o LEFT JOIN  customer c ON o.customerId = c.id ");
     console.log('rows:', rows);
     return (rows);
 }
