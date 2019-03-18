879e18d0d2858a7e9f9e2143a6a7c8af46d4bcfe
Pedro Nunes <pedroj.nunes0@gmail.com>
2019-03-18 03:36:18 -0300

    adiciona o CORS pra permitir requests

--- build/routes/order.route.js
+++ build/routes/order.route.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+//var cors = require('cors');
 var router = express_1.default.Router();
 var orderController = require('../controllers/order.controller');
 router.get('/', orderController.get);
